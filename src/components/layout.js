9283a0b1aeaa74e060c93b1cf79652643daa9941
eatdesignlove <eatdesignlove@gmail.com>
2019-01-19 20:16:05 +0900

    fix: fixed typo

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import Footer from './Footer'
+import Footer from './footer'
 import styles from './layout.module.scss'
 
 const Layout = ({ children }) => (
